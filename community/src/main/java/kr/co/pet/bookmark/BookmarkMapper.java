bf8e63929c12243b261572fc513a41a684cd101c
saparian0304 <anjp0304@gmail.com>
2022-08-19 12:35:28 +0900

    상세페이지 북마크기능 구현

--- community/src/main/java/kr/co/pet/bookmark/BookmarkMapper.java
+++ community/src/main/java/kr/co/pet/bookmark/BookmarkMapper.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import kr.co.pet.board.BoardVO;
+
 @Mapper
 public interface BookmarkMapper {
 
@@ -13,28 +15,28 @@ public interface BookmarkMapper {
 	 * @param vo BookmarkVO - member_no, board_no
 	 * @return  
 	 */
-	int bookmarked(BookMarkVO vo);
+	int bookmarked(BoardVO vo);
 	
 	/**
 	 * 북마크 생성
 	 * @param vo RecommendVO -  member_no, board_no
 	 * @return int
 	 */
-	int bookmark(BookMarkVO vo);		
+	int bookmark(BoardVO vo);		
 
 	/**
 	 * 북마크 해제
 	 * @param vo RecommendVO -  member_no, board_no
 	 * @return int
 	 */
-	int cancleBookmark(BookMarkVO vo);
+	int cancleBookmark(BoardVO vo);
 	
 	/**
 	 * 게시글의 북마크 수 카운트  
 	 * @param vo RecommendVO - board_no
 	 * @return int
 	 */
-	int getBookmarkCnt(BookMarkVO vo);
+	int getBookmarkCnt(BoardVO vo);
 	
 	/**
 	 * 회원의 북마크 수 카운트  
--- community/src/main/java/kr/co/pet/bookmark/BookmarkServiceImpl.java
+++ community/src/main/java/kr/co/pet/bookmark/BookmarkServiceImpl.java
@@ -3,8 +3,13 @@ package kr.co.pet.bookmark;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import kr.co.pet.board.BoardVO;
+import kr.co.pet.member.MemberVO;
 
 @Service
 public class BookmarkServiceImpl implements BookmarkService {
@@ -16,7 +21,7 @@ public class BookmarkServiceImpl implements BookmarkService {
 	 * true면 북마크, false는 북마크 해제
 	 */
 	@Override
-	public boolean bookmark(BookMarkVO vo) {
+	public boolean bookmark(BoardVO vo) {
 		boolean result = false;
 		if (mapper.bookmarked(vo) == 0) {
 			mapper.bookmark(vo);
@@ -26,12 +31,28 @@ public class BookmarkServiceImpl implements BookmarkService {
 		}
 		return result;
 	}
+	
+	/**
+	 * 북마크 여부 확인
+	 */
+	@Override
+	public boolean bookmarked(BoardVO vo, HttpSession sess) {
+		MemberVO mvo = (MemberVO)sess.getAttribute("loginInfo");
+		boolean flag = false;
+		if(mvo != null) {
+			vo.setMember_no(mvo.getMember_no());
+			if (mapper.bookmarked(vo) > 0)
+				flag = true;
+		}
+		System.out.println("service" + flag);
+		return flag;
+	}
 
 	/**
 	 * 게시글의 북마크 수 카운트
 	 */
 	@Override
-	public int getBookmarkCnt(BookMarkVO vo) {
+	public int getBookmarkCnt(BoardVO vo) {
 		return mapper.getBookmarkCnt(vo);
 	}
 
